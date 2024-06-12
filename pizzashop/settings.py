13ac943e97618e45d54553d7706549f76c38fa25
Jake Farrell <jake.farrell88@mail.dcu.ie>
2024-06-12 23:44:32 +0100

    staticfiles issue fix

--- pizzashop/settings.py
+++ pizzashop/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for pizzashop project.
 
@@ -13,7 +14,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
